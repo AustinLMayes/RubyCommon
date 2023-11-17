cebab3418af2d48862d0dafa28c84fdb7068d12f
Austin L Mayes <me@austinlm.me>
2023-11-17 03:26:22 -0600

    Add debug logger

--- lib/common/logging.rb
+++ lib/common/logging.rb
@@ -12,3 +12,14 @@ def error(msg)
     puts "#{Ansi.red "[ERROR]"} #{msg}"
     exit false
 end
+
+$debug = false
+
+def enable_debug
+    $debug = true
+end
+
+def debug(*msg)
+    return unless $debug
+    puts "#{Ansi.blue "[DEBUG]"} #{msg.join(" ")}"
+end
