ac926ebeae03fee3851ab64429e423c5f9841abd
Austin L Mayes <me@austinlm.me>
2024-07-14 21:35:53 -0500

    Support for more time formats

--- lib/common/time.rb
+++ lib/common/time.rb
@@ -3,18 +3,25 @@ module TimeUtils
 
     def parse_time(time_string)
         # Default to hours if no suffix is given
-        time_string += "h" unless time_string =~ /[mhd]/
-        # Parse the time string in short form (e.g. 1h30m)
+        time_string += "h" unless time_string =~ /[smhdwMy]/
         time = 0
-        time_string.scan(/(\d+)([mhd])/).each do |num, suffix|
+        time_string.scan(/(\d+)([smhdwMy])/).each do |num, suffix|
             num = num.to_i
             case suffix
+            when "s"
+                time += num.seconds
             when "m"
                 time += num.minutes
             when "h"
                 time += num.hours
             when "d"
                 time += num.days
+            when "w"
+                time += num.weeks
+            when "M"
+                time += num.months
+            when "y"
+                time += num.years
             end
         end
         time
