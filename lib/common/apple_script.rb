4a57f8e63ec60056d6403b8050c5cd72665942fe
Austin L Mayes <me@austinlm.me>
2024-07-14 21:34:56 -0500

    Send a notification center notification

--- lib/common/apple_script.rb
+++ lib/common/apple_script.rb
@@ -4,4 +4,9 @@ module AppleScript
     def run_script(script)
       system 'osascript', *script.split(/\n/).map { |line| ['-e', line] }.flatten
     end
+
+    def notification(title, text, sound = "Funk")
+      script = "display notification \"#{text}\" with title \"#{title}\" sound name \"#{sound}\""
+      run_script(script)
+    end
   end
